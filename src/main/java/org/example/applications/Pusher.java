7e4efaf827823df988dc85ac7849e835d789169c
Matthew Stafford <matthewstafford@Matthews-MacBook-Pro.local>
2024-02-18 13:50:54 -0500

    create and send message to queue

--- src/main/java/org/example/applications/Pusher.java
+++ src/main/java/org/example/applications/Pusher.java
@@ -4,18 +4,26 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.example.rabbitmq.QueueClient;
+import org.example.rabbitmq.RABBITMQ_CONSTANTS;
 
 import java.net.ConnectException;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
-public class Pusher implements Application{
+public class Pusher implements Application {
 
     private final static String QUEUE_NAME = "hello";
 
-    public void run() throws Exception{
-        try(QueueClient queueClient = new QueueClient()){
+    public void run() throws Exception {
+
+        try (QueueClient queueClient = QueueClient.builder()
+                .userName(RABBITMQ_CONSTANTS.USERNAME)
+                .password(RABBITMQ_CONSTANTS.PASSWORD)
+                .hostname(RABBITMQ_CONSTANTS.HOSTNAME)
+                .build()) {
             queueClient.init();
+            queueClient.createQueue("test");
+            queueClient.sendMessage("test","foo");
         }
     }
 }
