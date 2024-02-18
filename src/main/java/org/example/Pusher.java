22f524c5e96f32f2d1f4d3142aa93b813cf8b630
Matthew Stafford <matthewstafford@Matthews-MacBook-Pro.local>
2024-02-17 21:30:06 -0500

    sending messages

--- src/main/java/org/example/Pusher.java
+++ src/main/java/org/example/Pusher.java
@@ -6,45 +6,49 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.net.ConnectException;
-import java.sql.Time;
-import java.time.Instant;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Pusher {
 
     private final static String QUEUE_NAME = "hello";
 
-    public void push() throws IOException {
+    public void push() throws IOException, TimeoutException {
         final Optional<Connection> connection = getConnection();
+        System.out.println(connection.get().getPort());
         if (connection.isEmpty()) {
             System.out.println("Failed to establish connection, killing session!");
             throw new RuntimeException();
         }
         final Optional<Channel> channel = getChannelWithRetries(connection.get());
+
         if (channel.isEmpty()) {
             System.out.println("Failed to establish connection, killing session!");
             throw new RuntimeException();
         }
-        channel.get().queueDeclare(QUEUE_NAME, false, false, false, null);
         String message = "Hello World!";
         channel.get().basicPublish("", QUEUE_NAME, null, message.getBytes());
         System.out.println(" [x] Sent '" + message + "'");
-
+        channel.get().close();
+        connection.get().close();
     }
 
 
     private Optional<Connection> getConnection() {
         ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
         final int maxRetry = 100;
         final long sleepSeconds = TimeUnit.SECONDS.toMillis(5);
         int count = 0;
         while (count < maxRetry) {
-            factory.setHost("localhost");
+            factory.setHost("rabbitmq");
             try {
                 return Optional.of(factory.newConnection());
             } catch (ConnectException e) {
                 System.out.println(String.format("Failed to connect retrying in %s", sleepSeconds));
+                e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             }
